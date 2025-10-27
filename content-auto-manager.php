2b3ba448fafcb3cd865b80970cecd6bd3a161ba4
AI TOOL <admin@kdjingpai.com>
2025-10-27 13:16:28 +0800

    Add license information to README and create LICENSE.txt file

--- content-auto-manager.php
+++ content-auto-manager.php
@@ -6,7 +6,7 @@
  * Version: 1.0.0
  * Author: AI Productivity Tools
  * Author URI: https://www.kdjingpai.com/
- * License: GPL v2 or later
+ * License: GPLv2 or later
  * Text Domain: content-auto-manager
  */
 
